fb7a6d38834510b4fe0032aae4b785b4b7efa6d3
MHMighani <mhmighani.dev@gmail.com>
2026-05-14 11:05:01 +0330

    Updates

--- server/assets/_tanstack-start-manifest_v-BbmpkEBy.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/hermes/Desktop/amin/arc-snake-quest/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/arc-snake-quest/assets/index-BpvXUqi-.js"] }, "/": { filePath: "/home/hermes/Desktop/amin/arc-snake-quest/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/arc-snake-quest/assets/index-CMCmFVLP.js"] } }, clientEntry: "/arc-snake-quest/assets/index-BpvXUqi-.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-DybpAlV7.js";
-import "node:events";
-export {
-  w as default
-};
